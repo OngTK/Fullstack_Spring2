4882fee7dc797d1e71fadd68072f3027ac06cb3e
OngTK <dhdxorud@gmail.com>
2025-09-01 09:47:31 +0900

    Initial commit

---
+++ src/main/java/example/day01_250901/AppStart.java
@@ -0,0 +1,11 @@
+package example.day01_250901;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStart {
+    public static void main(String[] args) {
+        SpringApplication.run(AppStart.class);
+    }
+}
