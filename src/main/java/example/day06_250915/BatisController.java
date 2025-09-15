08c0d53cd2a47f9d497da6518c09c790838f957a
OngTK <dhdxorud@gmail.com>
2025-09-15 17:05:46 +0900

    250915_1616 myBatis 2

--- src/main/java/example/day06_250915/BatisController.java
+++ src/main/java/example/day06_250915/BatisController.java
@@ -1,12 +1,13 @@
 package example.day06_250915;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,27 +17,60 @@ public class BatisController {
     // [0] mapper 객체 DI
     private final BatisMapper batisMapper;
 
+
     // [1] create
-    public void create(){} // func end
+    @PostMapping
+    public ResponseEntity<Integer> create(@RequestBody StudentDto studentDto){
+        System.out.println("BatisController.create");
+        System.out.println("studentDto = " + studentDto);
+
+        int result = batisMapper.create(studentDto);
+        return ResponseEntity.status(200).body(result);
+    } // func end
+
 
     // [2] readAll
     @GetMapping
     public ResponseEntity<List<StudentDto>> readAll(){
+        System.out.println("BatisController.readAll");
+
         // batisMapper interFace의 readAll SQL문을 실행한 결과를 반환
         List<StudentDto> result = batisMapper.readAll();
-
         // ResponseEntity 응답객체로 반환
         return ResponseEntity.status(200).body(result);
     } // func end
 
+
     // [3] read
-    public void read(){} // func end
+    @GetMapping("/read")
+    public ResponseEntity< Map<String, Object> > read(@RequestParam int sno){
+        System.out.println("BatisController.read");
+        System.out.println("sno = " + sno);
+
+        Map<String, Object> result = batisMapper.read(sno);
+        return ResponseEntity.status(200).body(result);
+    } // func end
+
 
     // [4] update
-    public void update(){} // func end
+    @PutMapping
+    public ResponseEntity<Integer> update(@RequestBody StudentDto studentDto){
+        System.out.println("BatisController.update");
+        System.out.println("studentDto = " + studentDto);
+
+        int result = batisMapper.update(studentDto);
+        return ResponseEntity.status(200).body(result);
+    } // func end
+
 
     // [5] delete
-    public void delete(){} // func end
+    @DeleteMapping
+    public ResponseEntity<Integer> delete(@RequestParam int sno){
+        System.out.println("BatisController.delete");
+        System.out.println("sno = " + sno);
 
+        int result = batisMapper.delete(sno);
+        return ResponseEntity.status(200).body(result);
+    } // func end
 
 } // class end
--- src/main/java/example/day06_250915/BatisMapper.java
+++ src/main/java/example/day06_250915/BatisMapper.java
@@ -1,24 +1,45 @@
 package example.day06_250915;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
+import java.util.Map;
+
+/**
+ * <p> myBatis를 활용한 Mapping interFace
+ * @since 250915
+ * */
 
 // 해당 mybatis interface를 Spring bean에 등록하는 어노테이션 = DAO 역할
 @Mapper
 public interface BatisMapper {
 
     // [1] create
+    @Insert("insert into student(name, kor, math) VALUES(#{name},#{kor},#{math})")
+    int create(StudentDto studentDto);
+    // 성공한 레코드 수를 반환 : 1
+
 
     // [2] readAll
     @Select ("select * from student")
     List< StudentDto > readAll();
 
+
     // [3] read
+    @Select("select * from student where sno=#{sno}")
+    Map<String, Object> read(int sno); // func end
+
 
     // [4] update
+    @Update("update student set kor=#{kor}, math=#{math} where sno=#{sno}")
+    int update(StudentDto studentDto); // func end
+    // 성공한 레코드 수를 반환 : 1
+
 
     // [5] delete
+    @Delete("delete from student where sno=#{sno}")
+    int delete(int sno); // func end
+    // 성공한 레코드 수를 반환 : 1
+
 
 } // interface end
