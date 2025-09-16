db53f113b231b1188c63c576480238d99314d60e
OngTK <dhdxorud@gmail.com>
2025-09-16 12:13:25 +0900

    250916_1213 react 렌더링 3

--- src/main/reactapp/src/example/day01_250916/Component3.jsx
+++ src/main/reactapp/src/example/day01_250916/Component3.jsx
@@ -18,5 +18,32 @@ export default function Component3(props) {
     return (<>
         <div>{name} 입니다.</div>
         <div>{group}</div>
+
+        {/*다른 컴포넌트 포함시키기, props 속성 할당 */}
+        <SubCom1 key1="강형호" key2="38"/>
+    </>)
+} // func end
+
+
+// [ props ] 알아보기
+function SubCom1( props ){
+ 
+    const obj = {name : "배두훈", age : 40};
+    console.log(obj)
+
+    // [1] propes 확인하기
+    console.log(props)
+    // 결과 : {key1: 'value1', key2: '40'}
+    // props이 Object! 하나의 객체로 들어옴!
+
+    // [2] 구조 분해
+    const {key1, key2} = props
+
+    return(<> 
+        <h4>{obj.name}님의 나이는 {obj.age}입니다.</h4>
+        {/* props 속성 출력 */}
+        <h4>{props.key1}님의 나이는 {props.key2}입니다.</h4>
+        {/* 구조분해 결과 출력 */}
+        <h4>{key1}님의 나이는 {key2}입니다.</h4>
     </>)
 } // func end
