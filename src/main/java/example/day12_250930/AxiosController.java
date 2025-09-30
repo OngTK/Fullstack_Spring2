5ec8a847fb806b820631aaa3ec59ce5b0ed55062
OngTK <dhdxorud@gmail.com>
2025-09-30 18:20:17 +0900

    250930_1820 cros 설정

--- src/main/java/example/day12_250930/AxiosController.java
+++ src/main/java/example/day12_250930/AxiosController.java
@@ -1,8 +1,13 @@
 package example.day12_250930;
 
 import jakarta.servlet.http.HttpSession;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
 
@@ -11,7 +16,7 @@ import java.util.Map;
 @Log4j2
 public class AxiosController {
 
-    // [1]
+    // [1] axios 통신
     @GetMapping
     public int axios1 (){
         return 10; // 임의 값
@@ -36,11 +41,34 @@ public class AxiosController {
         if(object == null) return false;
         // 로그인 상태
         return true;
-
     } // func end
-} // class end
+    
+    // [4] form 정보 받기
+    @PostMapping("/form")
+    public boolean axios4 (@RequestParam Map<String, String> map){
+        System.out.println("AxiosController.axios4");
+        System.out.println("map = " + map);
 
-/*
-*
-*
-* */
+        return true;
+    } // func end
+    
+    // [5] 첨부파일 form 정보 받기
+    @PostMapping("/formdata")
+    public boolean axios5 (@RequestParam MultipartFile file){
+        System.out.println("AxiosController.axios5");
+        System.out.println("file = " + file);
+
+        return true;
+    } // func end
+
+    
+} // class end
+//
+//@Repository
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//class AxiosDto{
+//    String id;
+//    String password;
+//}
--- src/main/reactapp/src/example/day08_250930/Component15.jsx
+++ src/main/reactapp/src/example/day08_250930/Component15.jsx
@@ -1,6 +1,7 @@
 import Input from '@mui/joy/Input'
 import Button from '@mui/joy/Button'
 import axios from 'axios'
+import { use, useRef } from 'react'
 
 export default function Component15(props) {
 
@@ -8,7 +9,7 @@ export default function Component15(props) {
     const axios1 = async () => {
         try {
             const r = await axios.get("http://localhost:8080/axios")
-            const d = await r.data;
+            const d =  r.data;
             console.log("[1] : ", d)
         } catch (e) {
             console.log(e)
@@ -19,10 +20,10 @@ export default function Component15(props) {
     const axios2 = async () => {
         try {
             const obj = { id: "admin", pwd: "qwer1234" }
-            const option = {withCredentials : true} //HTTP 인증, Session 유지 true 
+            const option = { withCredentials: true } //HTTP 인증, Session 유지 true 
             // post는 3번째 자리에 option ==> .post( URL, BODY, OPTION )
             const r = await axios.post("http://localhost:8080/axios", obj, option)
-            const d = await r.data;
+            const d =  r.data;
             console.log("[2] : ", d)
         } catch (e) {
             console.log(e)
@@ -32,13 +33,55 @@ export default function Component15(props) {
     // [3] 내 정보 조회
     const axios3 = async () => {
         try {
-            const option = {withCredentials : true} // get은 2번째 자리에 option
+            const option = { withCredentials: true } // get은 2번째 자리에 option
             // .get( URL, OPTION )
             const r = await axios.get("http://localhost:8080/axios/info", option)
-            const d = await r.data;
+            const d =  r.data;
             console.log("[3] : ", d)
         } catch (e) { console.log(e) }
     }
+
+    // fetch는 기본 전송타입이 form
+    // axios는 기본 전송타입이 json
+
+    // [4] 일반 폼
+    // form 전송시, form 안에 name으로 매핑이 진행됨
+    const formRef1 = useRef();
+    const axios4 = async () => {
+        try{
+            // [4.1] useRef 가 참조중인 dom 객체 가져오기
+            const form = formRef1.current;
+            // [4.2] headers 정보 입력 
+            const option = { headers : { "Content-type" : "application/x-www.form-urlencoded" } } 
+            // [4.3] axios 통신
+            const r = await axios.post("http://localhost:8080/axios/form",form,option);
+            const d = r.data;
+            console.log("[4] : " , d)
+
+        }catch(e){
+            console.log(e)
+        }
+    }
+
+    // [5] 첨부파일 폼
+    const formRef2 = useRef()
+    const axios5 = async () => {
+        try{
+            // [5.1] uesRef로 참조중인 dom 객체 가져오기
+            const form = formRef2.current;
+            // [5.2] Form 내에 data를 byte 형식으로 변환
+            const formData = new FormData(form);
+            // [5.3] headers 정보 
+            const option = { headers: { "Content-Type": "multipart/form-data" }  }
+            // [5.4] axios
+            const r = await axios.post("http://localhost:8080/axios/formdata",formData, option)
+            const d = r.data;
+            console.log("[5] : " , d)
+        } catch(e){
+            console.log(e)
+        }
+    }
+
 
     return (<>
         <h3>Component15 </h3>
@@ -47,6 +90,16 @@ export default function Component15(props) {
         <Button onClick={axios2}> axios2 버튼</Button>
         <br />
         <Button onClick={axios3}> axios3 버튼</Button>
+        <br />
+        <form ref={formRef1}>
+            <Input name="id"/>
+            <Input name="password"/>
+            <Button type='button' onClick={axios4}>axios4</Button>
+        </form>
+        <form ref={formRef2}>
+            <Input type='file' name='file'/>
+            <Button type='button' onClick={axios5}>axios5</Button>    
+        </form>
 
     </>)
 } // Comp end
