d384c858ee293f19e60253dd15690412f3a805f0
OngTK <dhdxorud@gmail.com>
2025-10-13 12:17:34 +0900

    251013_1115 xml update/delete

--- src/main/java/example/day13_251013/XmlController.java
+++ src/main/java/example/day13_251013/XmlController.java
@@ -17,9 +17,20 @@ public class XmlController {
     // [1] 등록
     @PostMapping
     public ResponseEntity< ? > save(@RequestBody StudentDto studentDto){
-        // 제네릭 ? : 모든 타입 (와일드카드)6
-        xmlMapper.save(studentDto);
-        return ResponseEntity.ok(true);
+        // 제네릭 ? : 모든 타입 (와일드카드)
+
+        System.out.println("studentDto = " + studentDto);
+
+        int result = xmlMapper.save(studentDto);
+
+        System.out.println("studentDto = " + studentDto);
+
+        if(result == 1 ){
+            return ResponseEntity.ok(studentDto.getSno());
+        } else{
+            return ResponseEntity.status(500).body(false);
+        }
+
     } // func end
 
     // [2] 전체조회
@@ -27,6 +38,29 @@ public class XmlController {
     public ResponseEntity< ? > findAll( ){
         List<StudentDto> result = xmlMapper.findAll();
         return ResponseEntity.ok(result);
+    }
+
+    // [3] 개별조회
+    @GetMapping("/find")
+    public ResponseEntity< ? > find(int sno) {
+        StudentDto result = xmlMapper.find(sno);
+        return ResponseEntity.ok(result);
+    }
+
+    // [4] 개별 삭제
+    @DeleteMapping
+    public ResponseEntity<?> delete(int sno){
+        xmlMapper.delete(sno);
+        return ResponseEntity.ok(true);
+    }
+
+    // [5] 수정
+    @PutMapping
+    public ResponseEntity<?> update(@RequestBody StudentDto studentDto){
+        System.out.println("XmlController.update");
+        System.out.println("studentDto = " + studentDto);
+        xmlMapper.update(studentDto);
+        return ResponseEntity.ok(true);
     }
 
 } // class end
--- src/main/java/example/day13_251013/XmlMapper.java
+++ src/main/java/example/day13_251013/XmlMapper.java
@@ -1,6 +1,9 @@
 package example.day13_251013;
 
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.InsertProvider;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 
 import java.util.List;
 
@@ -10,9 +13,20 @@ public interface XmlMapper {
     // [1] 등록
     // AsIs - @Insert("SQL")
     // ToBe - 추상메소드를 매핑하는 XML 파일에서 SQL 작성
+//    @Insert("INSERT INTO student(name, kor, math) values( #{name}, #{kor}, #{math});")
+//    @Options(useGeneratedKeys = true, keyProperty = "sno") //Auto_Increament PK를 반환
     int save(StudentDto dto);
 
     // [2] 전체 조회
     List<StudentDto> findAll();
+
+    // [3] 개별 조회
+    StudentDto find (int sno);
+
+    // [4] 개별 삭제
+    int delete (int sno);
+
+    // [5] 수정
+    int update(StudentDto studentDto);
 
 } // interface end
