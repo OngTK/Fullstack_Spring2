3a697fe9bbf3f04e451c254c22f99afe29733a34
OngTK <dhdxorud@gmail.com>
2025-10-13 10:40:00 +0900

    251013_1023 서버시작시 sql 시작하는 기능

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,60 @@
+-- --------------------------------------- 실습1 ----------------------------------------
+drop table if exists products;
+CREATE TABLE products (
+    product_id INT PRIMARY KEY AUTO_INCREMENT, -- 상품 ID (자동 증가)
+    product_name VARCHAR(255) NOT NULL,        -- 상품명
+    stock_quantity INT NOT NULL                -- 재고 수량
+);
+
+-- --------------------------------------- day06 example ----------------------------------------
+-- 학생 테이블
+drop table if exists student;
+CREATE TABLE student (
+    sno INT AUTO_INCREMENT,              -- 학생 번호 (자동 증가)
+    name VARCHAR(50) NOT NULL,           -- 이름
+    kor INT NOT NULL,                    -- 국어 점수
+    math INT NOT NULL,                    -- 수학 점수
+    CONSTRAINT  PRIMARY KEY (sno)  -- 기본키 제약 조건 추가
+);
+
+-- --------------------------------------- day07 boardservice 13 ----------------------------------------
+
+drop table if exists board;
+create table board(
+        bno int auto_increment ,
+    bcontent longtext not null ,
+    bwriter varchar(30) not null ,
+    constraint primary key(bno)
+);
+
+-- --------------------------------------- day09 250925 ----------------------------------------
+drop table if exists trans;
+CREATE TABLE trans(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    money INT UNSIGNED DEFAULT 0
+);
+
+-- --------------------------------------- 실습3  ----------------------------------------
+-- FK 먼저 삭제할 것
+DROP TABLE if exists rentals;
+DROP TABLE if exists books;
+
+-- 1. 책 테이블
+CREATE TABLE books (
+    id INT NOT NULL AUTO_INCREMENT ,
+    title VARCHAR(255) NOT NULL,
+    stock INT NOT NULL DEFAULT 0,
+    PRIMARY KEY (id)
+);
+
+-- 2. 대출 기록 테이블
+CREATE TABLE rentals (
+    id INT NOT NULL AUTO_INCREMENT,
+    book_id INT NOT NULL,
+    member VARCHAR(100) NOT NULL,
+    rent_date DATETIME DEFAULT NOW(),
+    return_date DATETIME NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (book_id) REFERENCES books(id)
+);
