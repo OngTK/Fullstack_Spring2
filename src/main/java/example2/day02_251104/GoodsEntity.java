d8d7030e54eab17f0546beca68b8590b0d1a200b
OngTK <dhdxorud@gmail.com>
2025-11-04 10:24:34 +0900

    251104_1024 JPA

---
+++ src/main/java/example2/day02_251104/GoodsEntity.java
@@ -0,0 +1,29 @@
+package example2.day02_251104;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity                     // Entity 삽입
+@Table(name="goods")        // 테이블명 정의, 생략 시 클래스 명으로 table 명이 정의
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class GoodsEntity {
+
+    @Id                     // PK 선언
+    @GeneratedValue( strategy = GenerationType.IDENTITY )    // auto_increment 주입, Oracle에서는 지원하지 않음
+    private int gno;        // 제품 번호
+
+    // @Column(속성명=값, 속성명=값, ...)
+    @Column( nullable = false, length = 100)         // null 불가, 글자수 100 지정
+    private String gname;   // 제품명
+    
+    @Column(nullable = true)            // null 가능 = default
+    private int gprice;     // 제품 가격
+
+    @Column( columnDefinition = "varchar(100) default '제품설명' not null")
+    // SQL을 직접 선언
+    private String gdesc;   // 제품 설명
+
+} // class end
